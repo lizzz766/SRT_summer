3c183aa62b33319317a9ddd19697aac88ab1dec8
lizzz <1337409216@qq.com>
2022-07-08 19:43:31 +0800

    1

--- git_test01.py
+++ git_test01.py
@@ -1,3 +1,19 @@
+import gym
+import time
+env = gym.make("BreakoutNoFrameskip-v4")
+
+print("Observation Space: ", env.observation_space)
+print("Action Space       ", env.action_space)
+
+
+obs = env.reset()
+
+for i in range(1000):
+    action = env.action_space.sample()
+    obs, reward, done, info = env.step(action)
+    env.render()
+    time.sleep(0.01)
+env.close()
 '''
 Note foe git
 1.配置：默认编辑器(VIM,VSCode)
@@ -10,5 +26,9 @@ Note foe git
     添加文件：
     git add
     不知道改啥了
-    
+
+3.文件分支
+
+
+
 '''
